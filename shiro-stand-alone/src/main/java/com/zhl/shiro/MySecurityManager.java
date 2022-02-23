5b341c79ab869f182e9f73e279ad37d67e6fb7ec
zhl <707169146@qq.com>
2022-02-23 12:23:11 +0800

    用户认证异常场景代码优化

--- shiro-stand-alone/src/main/java/com/zhl/shiro/MySecurityManager.java
+++ shiro-stand-alone/src/main/java/com/zhl/shiro/MySecurityManager.java
@@ -2,6 +2,8 @@ package com.zhl.shiro;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.text.IniRealm;
@@ -31,8 +33,11 @@ public class MySecurityManager {
             System.out.println("状态信息:" + subject.isAuthenticated());
             subject.login(token);
             System.out.println("状态信息:" + subject.isAuthenticated());
-        } catch (AuthenticationException e) {
-            e.printStackTrace();
+        } catch (UnknownAccountException e) {
+            System.out.println("用户名不存在: " + token.getUsername());
+        }
+        catch (IncorrectCredentialsException e) {
+            System.out.println("用户: " + token.getUsername() + " 密码不正确");
         }
     }
 }
