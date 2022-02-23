729bb977b651ca1feb7086f50ba3b5b896cceabb
zhl <707169146@qq.com>
2022-02-23 16:25:07 +0800

    shiro md5 加密验证

---
+++ shiro-stand-alone/src/main/java/com/zhl/shiro/Md5Realm.java
@@ -0,0 +1,52 @@
+package com.zhl.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+/**
+ * Md5加密算法的realm
+ *
+ * @author 70716
+ */
+public class Md5Realm extends AuthorizingRealm {
+    private static String USERNAME = "admin";
+
+    private static String MD5STR = "21232f297a57a5a743894a0e4a801fc3";
+
+    private static String MD5ANDSALT = "25a96b83f1593475d1b40da779371b08";
+
+    private static String MD5ANDSALTANDHASH = "bedff84cc9e93aa45b265fe235dbacce";
+
+    private static String SALT = "weihuashangyeyinhang";
+
+    private static int HASHINT = 1024;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+//    @Override
+//    public void setCredentialsMatcher(CredentialsMatcher credentialsMatcher) {
+//        super.setCredentialsMatcher(credentialsMatcher);
+//    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //模拟数据库匹配
+        String principal = (String) token.getPrincipal();
+        if (USERNAME.equals(principal)) {
+//            return new SimpleAuthenticationInfo(principal, MD5STR, this.getName());
+//            return new SimpleAuthenticationInfo(principal, MD5ANDSALT, ByteSource.Util.bytes(SALT), this.getName());
+            return new SimpleAuthenticationInfo(principal, MD5ANDSALTANDHASH, ByteSource.Util.bytes(SALT), this.getName());
+        }
+        return null;
+    }
+}
